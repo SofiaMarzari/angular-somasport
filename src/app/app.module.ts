f22e50f746ff6c26b9d343cfd831222b7e08cd3b
SofiaMarzari <sofiamarzari3544@gmail.com>
2020-11-27 13:23:00 -0300

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UsuarioBtnComponent } from './usuario-btn/usuario-btn.component';
 import { CarritoComprasComponent } from './carrito-compras/carrito-compras.component';
+import { GaleriaImagenesComponent } from './galeria-imagenes/galeria-imagenes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsuarioBtnComponent,
-    CarritoComprasComponent
+    CarritoComprasComponent,
+    GaleriaImagenesComponent
   ],
   imports: [
     BrowserModule
